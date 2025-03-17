ace1d5888f321f854cb5131c930322b912e21037
amitgupta141 <ak1864953@gmail.com>
2025-03-17 23:41:11 +0530

    faq and sponsor pages

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -2,20 +2,39 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Components.css"; 
+import Accordion from "./Faq/Accordian";
 
 const Faq = () => {
   return (
     <div className="mainPage">
       <div className="Parallax">
         <div className="faq-header">
+       
           <h1 className="faq-title">
-            FAQ 
+
+            <span>
+            FAQ ?
+            <FontAwesomeIcon icon={faBookOpen} />
+            </span>
+         
             <span className="faq-icon">
-              <FontAwesomeIcon icon={faBookOpen} />
+
+              <Accordion />
             </span>
           </h1>
+        
+        
         </div>
+
+       
+
+      
+
+     
+       
       </div>
+
+     
     </div>
   );
 };
---
+++ src/components/Faq/Accordian.js
@@ -0,0 +1,21 @@
+
+import AccordianItem from "./AccordianItem.js"
+
+const Accordian = () => {
+    const faqData = [
+      { question: "Q: What exactly is sankalan?", answer: "Sankalan is the annual technical festival of Department of Computer Science, University of Delhi. The fest blends a plethora of events including coding, quizzes, gaming, debates and a lot more events, it has something in store for everyone. It is entirely organised by the DUCS student body. To know more about it, please visit our about page." },
+      { question: "Q: when is sankalan 2025 scheduled?", answer: "useState is a React Hook that allows you to add state to functional components." },
+      { question: "Q: who can participate?", answer: "JSX is a syntax extension for JavaScript that allows you to write HTML-like code inside JavaScript files." },
+      { question: "Q: who can participate?", answer: "Props are used to pass data from a parent component to a child component." },
+    ];
+  
+    return (
+      <div className="accordion-container">
+        {faqData.map((faq, index) => (
+          <AccordianItem key={index} question={faq.question} answer={faq.answer} />
+        ))}
+      </div>
+    );
+  };
+  
+  export default Accordian;
