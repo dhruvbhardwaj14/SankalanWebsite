530169114a9296e8e0d05ca6cf1fcb918550f046
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2025-03-20 18:15:08 +0530

    before deployment

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -3,28 +3,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHandsHelping } from "@fortawesome/free-solid-svg-icons";
 import "../styles/Components.css"; 
 import "../styles/Sponsors.css"; 
+import c1 from "../assets/PNG/c1.png"
+import c2 from "../assets/PNG/c2.png"
+import c3 from "../assets/PNG/c3.png"
+import c4 from "../assets/PNG/c4.png"
 const Sponsor = () => {
   const sponsors = [
-    { id: 1, name: "Company A", logo: "https://play-lh.googleusercontent.com/VCZKZgA_VP5PVHNCDJ3rWwtqDAuQwLKfuP0u5d9PiWef_IYhI176pRlXYLPmMlbpUlav=w480-h960-rw" },
-    { id: 2, name: "Company B", logo: "/sponsors/c1.png" },
-    { id: 3, name: "Company C", logo: "/sponsors/c2.png" },
-    { id: 4, name: "Company D", logo: "/sponsors/c3.png" },
-    { id: 5, name: "Company E", logo: "/sponsors/c4.png" },
-    { id: 6, name: "Company F", logo: "https://play-lh.googleusercontent.com/VCZKZgA_VP5PVHNCDJ3rWwtqDAuQwLKfuP0u5d9PiWef_IYhI176pRlXYLPmMlbpUlav=w480-h960-rw" },
-    { id: 7, name: "Company G", logo: "/sponsors/c1.png" },
-    { id: 8, name: "Company H", logo: "/sponsors/c2.png" },
-    { id: 9, name: "Company I", logo: "/sponsors/c3.png" },
-    { id: 10, name: "Company J", logo: "/sponsors/c4.png" },
-    { id: 11, name: "Company K", logo: "https://play-lh.googleusercontent.com/VCZKZgA_VP5PVHNCDJ3rWwtqDAuQwLKfuP0u5d9PiWef_IYhI176pRlXYLPmMlbpUlav=w480-h960-rw" },
-    { id: 12, name: "Company L", logo: "/sponsors/c1.png" },
-    { id: 13, name: "Company M", logo: "/sponsors/c2.png" },
-    { id: 14, name: "Company N", logo: "/sponsors/c3.png" },
-    { id: 15, name: "Company O", logo: "/sponsors/c4.png" },
-    { id: 16, name: "Company P", logo: "https://play-lh.googleusercontent.com/VCZKZgA_VP5PVHNCDJ3rWwtqDAuQwLKfuP0u5d9PiWef_IYhI176pRlXYLPmMlbpUlav=w480-h960-rw" },
-    { id: 17, name: "Company Q", logo: "/sponsors/c1.png" },
-    { id: 18, name: "Company R", logo: "/sponsors/c2.png" },
-    { id: 19, name: "Company S", logo: "/sponsors/c3.png" },
-    { id: 20, name: "Company T", logo: "/sponsors/c4.png" },
+    { id: 1, name: "Company A", logo: c1 },
+    { id: 2, name: "Company B", logo: c1 },
+    { id: 3, name: "Company C", logo:c2 },
+    { id: 4, name: "Company D", logo: c3 },
+    { id: 5, name: "Company E", logo:c4 },
+    { id: 6, name: "Company F", logo: c1 },
+    { id: 7, name: "Company G", logo: c1 },
+    { id: 8, name: "Company H", logo:c2 },
+    { id: 9, name: "Company I", logo: c3 },
+    { id: 10, name: "Company J", logo:c4 },
+    { id: 11, name: "Company K", logo: c1 },
+    { id: 12, name: "Company L", logo: c1 },
+    { id: 13, name: "Company M", logo:c2 },
+    { id: 14, name: "Company N", logo: c3 },
+    { id: 15, name: "Company O", logo:c4 },
+    { id: 16, name: "Company P", logo: c1 },
+    { id: 17, name: "Company Q", logo: c1 },
+    { id: 18, name: "Company R", logo:c2 },
+    { id: 19, name: "Company S", logo: c3 },
+    { id: 20, name: "Company T", logo:c4 },
   ];
   return (
     <div className="mainPage">
