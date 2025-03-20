b19fa0047eb4f29f51cf666f3aae5ca86daebb6d
dhruvbhardwaj14 <dhruv9891891827@gmail.com>
2025-03-20 18:23:12 +0530

    Favicon changed

--- src/components/HomeComponents/Testimonials.js
+++ src/components/HomeComponents/Testimonials.js
@@ -23,31 +23,6 @@ const eventHighlights = [
 ];
 
 const Testimonials = () => {
-  const sliderSettings = {
-    dots: true,
-    infinite: true,
-    speed: 700,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    centerMode: true,
-    centerPadding: "0",
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
-  };
   return (
     <section className="ai-initiatives-section">
       <h2>
